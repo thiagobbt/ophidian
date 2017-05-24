104fdb0655872c60fe6beec9ef6fb8e087f6d23c
Renan Oliveira Netto <renan.o.netto@gmail.com>
2017-05-24 14:12:56 -0300

    multirow cells only being placed in cells with multiple size

--- ophidian/legalization/Subrows.cpp
+++ ophidian/legalization/Subrows.cpp
@@ -8,24 +8,29 @@ Subrows::Subrows(const circuit::Netlist & netlist, const floorplan::Floorplan & 
     createSubrows();
 }
 
-void Subrows::createSubrows()
+void Subrows::createSubrows(unsigned rowsPerCell)
 {
     subrows_.clear();
     subrowsRtree_.clear();
 
+    unsigned rowIndex = 0;
     for (auto rowIt = floorplan_.rowsRange().begin(); rowIt != floorplan_.rowsRange().end(); ++rowIt) {
-        auto subrow = subrows_.add();
-        auto rowOrigin = floorplan_.origin(*rowIt);
-        auto rowUpperCorner = floorplan_.rowUpperRightCorner(*rowIt);
-        rowUpperCorner.x(rowUpperCorner.x() + rowOrigin.x());
-        rowUpperCorner.y(rowUpperCorner.y() + rowOrigin.y());
-        subrowOrigins_[subrow] = rowOrigin;
-        subrowUpperCorners_[subrow] = rowUpperCorner;
-        subrowCapacities_[subrow] = rowUpperCorner.x() - rowOrigin.x();
+        if (rowIndex % rowsPerCell == 0) {
+            auto subrow = subrows_.add();
+            auto rowOrigin = floorplan_.origin(*rowIt);
 
-        geometry::Box subrowBox(geometry::Point(units::unit_cast<double>(rowOrigin.x()), units::unit_cast<double>(rowOrigin.y())),
-                                geometry::Point(units::unit_cast<double>(rowUpperCorner.x()), units::unit_cast<double>(rowUpperCorner.y())));
-        subrowsRtree_.insert(RtreeNode(subrowBox, subrow));
+            auto rowUpperCorner = floorplan_.rowUpperRightCorner(*rowIt);
+            rowUpperCorner.x(rowOrigin.x() + rowUpperCorner.x());
+            rowUpperCorner.y(rowOrigin.y() + (rowsPerCell * rowUpperCorner.y()));
+            subrowOrigins_[subrow] = rowOrigin;
+            subrowUpperCorners_[subrow] = rowUpperCorner;
+            subrowCapacities_[subrow] = rowUpperCorner.x() - rowOrigin.x();
+
+            geometry::Box subrowBox(geometry::Point(units::unit_cast<double>(rowOrigin.x()), units::unit_cast<double>(rowOrigin.y())),
+                                    geometry::Point(units::unit_cast<double>(rowUpperCorner.x()), units::unit_cast<double>(rowUpperCorner.y())));
+            subrowsRtree_.insert(RtreeNode(subrowBox, subrow));
+        }
+        rowIndex++;
     }
 
     for (auto cellIt = netlist_.begin(circuit::Cell()); cellIt != netlist_.end(circuit::Cell()); ++cellIt) {
@@ -52,14 +57,14 @@ void Subrows::createSubrows()
                     geometry::Box rightSubrowBox(geometry::Point(units::unit_cast<double>(subrowOrigins_[rightSubrow].x()), units::unit_cast<double>(subrowOrigins_[rightSubrow].y())),
                                                  geometry::Point(units::unit_cast<double>(subrowUpperCorners_[rightSubrow].x()), units::unit_cast<double>(subrowUpperCorners_[rightSubrow].y())));
 
-//                    std::cout << "cell box " << cellBox.min_corner().x() << ", " << cellBox.min_corner().y() << " -> " <<
-//                                 cellBox.max_corner().x() << ", " << cellBox.max_corner().y() << std::endl;
-//                    std::cout << "original subrow " << subrowOrigins_[subrow].x() << ", " << subrowOrigins_[subrow].y() << " -> " <<
-//                              subrowUpperCorners_[subrow].x() << ", " << subrowUpperCorners_[subrow].y() << std::endl;
-//                    std::cout << "left subrow " << subrowOrigins_[leftSubrow].x() << ", " << subrowOrigins_[leftSubrow].y() << " -> " <<
-//                              subrowUpperCorners_[leftSubrow].x() << ", " << subrowUpperCorners_[leftSubrow].y() << std::endl;
-//                    std::cout << "right subrow " << subrowOrigins_[rightSubrow].x() << ", " << subrowOrigins_[rightSubrow].y() << " -> " <<
-//                              subrowUpperCorners_[rightSubrow].x() << ", " << subrowUpperCorners_[rightSubrow].y() << std::endl;
+                    //                    std::cout << "cell box " << cellBox.min_corner().x() << ", " << cellBox.min_corner().y() << " -> " <<
+                    //                                 cellBox.max_corner().x() << ", " << cellBox.max_corner().y() << std::endl;
+                    //                    std::cout << "original subrow " << subrowOrigins_[subrow].x() << ", " << subrowOrigins_[subrow].y() << " -> " <<
+                    //                              subrowUpperCorners_[subrow].x() << ", " << subrowUpperCorners_[subrow].y() << std::endl;
+                    //                    std::cout << "left subrow " << subrowOrigins_[leftSubrow].x() << ", " << subrowOrigins_[leftSubrow].y() << " -> " <<
+                    //                              subrowUpperCorners_[leftSubrow].x() << ", " << subrowUpperCorners_[leftSubrow].y() << std::endl;
+                    //                    std::cout << "right subrow " << subrowOrigins_[rightSubrow].x() << ", " << subrowOrigins_[rightSubrow].y() << " -> " <<
+                    //                              subrowUpperCorners_[rightSubrow].x() << ", " << subrowUpperCorners_[rightSubrow].y() << std::endl;
 
                     subrowsRtree_.remove(subrowNode);
                     subrows_.erase(subrow);
--- test/legalization/multirow_abacus_test.cpp
+++ test/legalization/multirow_abacus_test.cpp
@@ -8,12 +8,12 @@ TEST_CASE_METHOD(MultirowAbacusFixture, "Legalization: legalizing small circuit 
     std::vector<ophidian::util::Location> expectedLocations = {
         ophidian::util::Location(0.7, 3.0),
         ophidian::util::Location(1.7, 3.0),
-        ophidian::util::Location(2.0, 2.0),
+        ophidian::util::Location(2.5, 2.0),
         ophidian::util::Location(4.0, 3.0),
+        ophidian::util::Location(4.0, 0.0),
         ophidian::util::Location(3.0, 0.0),
-        ophidian::util::Location(3.0, 1.0),
         ophidian::util::Location(4.0, 1.0),
-        ophidian::util::Location(4.0, 2.0),
+        ophidian::util::Location(3.5, 2.0),
     };
 
     ophidian::legalization::MultirowAbacus multirowAbacus(netlist_, floorplan_, placement_, placementMapping_);
@@ -22,7 +22,7 @@ TEST_CASE_METHOD(MultirowAbacusFixture, "Legalization: legalizing small circuit 
     std::vector<ophidian::util::Location> cellLocations;
     for (auto cellIt = netlist_.begin(ophidian::circuit::Cell()); cellIt != netlist_.end(ophidian::circuit::Cell()); ++cellIt) {
         cellLocations.push_back(placement_.cellLocation(*cellIt));
-//        std::cout << cellLocations.back().x() << ", " << cellLocations.back().y() << std::endl;
+        std::cout << cellLocations.back().x() << ", " << cellLocations.back().y() << std::endl;
     }
 
     REQUIRE(cellLocations.size() == expectedLocations.size());
