ba2ee5eaceff784cdf560f721a546a4af0fbb1f5
Thiago Barbato <thiagocbbt@gmail.com>
2018-04-05 11:40:40 -0300

    Change ICCAD2017 score to use double + some changes

--- ophidian/legalization/iccad2017SolutionQuality.cpp
+++ ophidian/legalization/iccad2017SolutionQuality.cpp
@@ -9,41 +9,43 @@ ICCAD2017SolutionQuality::ICCAD2017SolutionQuality(ophidian::design::Design &des
     mInitialLocations(design.netlist().makeProperty<util::Location>(ophidian::circuit::Cell())),
     mInitialFixed(design.netlist().makeProperty<bool>(ophidian::circuit::Cell())),
     mDesign(design),
-    mOriginalDesign(originalDesign)
+    mOriginalDesign(originalDesign),
+    mRowHeight(mDesign.floorplan().siteUpperRightCorner(*mDesign.floorplan().sitesRange().begin()).toPoint().y())
 {
-    for(auto cellIt = design.netlist().begin(ophidian::circuit::Cell()); cellIt != design.netlist().end(ophidian::circuit::Cell()); cellIt++)
+    for (auto cellIt = design.netlist().begin(ophidian::circuit::Cell()); cellIt != design.netlist().end(ophidian::circuit::Cell()); cellIt++)
     {
         mInitialFixed[*cellIt] = design.placement().isFixed(*cellIt);
         mInitialLocations[*cellIt] = design.placement().cellLocation(*cellIt);
     }
-    mRowHeight = mDesign.floorplan().siteUpperRightCorner(*mDesign.floorplan().sitesRange().begin()).toPoint().y();
+
+    // mRowHeight = mDesign.floorplan().siteUpperRightCorner(*mDesign.floorplan().sitesRange().begin()).toPoint().y();
 }
 
-float ICCAD2017SolutionQuality::avgMovementScore(){
+double ICCAD2017SolutionQuality::avgMovementScore() {
     std::unordered_map<double, std::pair<double, std::size_t>> cellHeight2Counter;
-    for(auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++)
-        if(!mDesign.placement().isFixed(*cellIt))
+    for (auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++)
+        if (!mDesign.placement().isFixed(*cellIt))
         {
             auto cellHeight = mDesign.placementMapping().geometry(*cellIt)[0].max_corner().y() - mDesign.placementMapping().geometry(*cellIt)[0].min_corner().y();
             double displacement = cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt]);
             auto displacementHeightCounter = cellHeight2Counter.find(cellHeight);
-            if(displacementHeightCounter == cellHeight2Counter.end())
+            if (displacementHeightCounter == cellHeight2Counter.end())
                 cellHeight2Counter[cellHeight] = std::pair<double, std::size_t>(displacement, 1);
             else
                 cellHeight2Counter[cellHeight] = std::pair<double, size_t>(cellHeight2Counter[cellHeight].first + displacement, cellHeight2Counter[cellHeight].second + 1);
         }
-    float averageDisplacement = 0;
-    for(auto displacementCounter : cellHeight2Counter)
+    double averageDisplacement = 0;
+    for (auto displacementCounter : cellHeight2Counter)
         averageDisplacement += (displacementCounter.second.first/displacementCounter.second.second);
     averageDisplacement = (averageDisplacement / mRowHeight) / cellHeight2Counter.size();
     return averageDisplacement;
 }
 
-float ICCAD2017SolutionQuality::maxMovementScore(){
+double ICCAD2017SolutionQuality::maxMovementScore() {
     return 1 + (maximumCellMovement()/100.0) * fmm();
 }
 
-float ICCAD2017SolutionQuality::hpwlScore(){
+double ICCAD2017SolutionQuality::hpwlScore() {
     double originalHPWL = 0;
 
     for (auto it = mOriginalDesign.netlist().begin(circuit::Net()); it < mOriginalDesign.netlist().end(circuit::Net()); it++) {
@@ -56,23 +58,23 @@ float ICCAD2017SolutionQuality::hpwlScore(){
         currentHPWL += hpwl(mDesign, *it);
     }
 
-    float score = std::max((currentHPWL - originalHPWL) / originalHPWL, 0.0) * (1 + std::max(0.0 /* beta * fof */, 0.2));
+    double score = std::max((currentHPWL - originalHPWL) / originalHPWL, 0.0) * (1 + std::max(BETA * fof(), 0.2));
 
     return score;
 }
 
-double ICCAD2017SolutionQuality::totalDisplacement(){
+double ICCAD2017SolutionQuality::totalDisplacement() {
     double displacement = 0.0;
-    for(auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++)
+    for (auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++)
         displacement+= cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt]);
     return displacement;
 }
 
-double ICCAD2017SolutionQuality::avgDisplacement(){
+double ICCAD2017SolutionQuality::avgDisplacement() {
     double displacement = 0.0;
     unsigned int amount = 0;
-    for(auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++){
-        if(mInitialFixed[*cellIt] == false){
+    for (auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++) {
+        if (mInitialFixed[*cellIt] == false) {
             displacement+= cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt]);
             amount++;
         }
@@ -80,37 +82,48 @@ double ICCAD2017SolutionQuality::avgDisplacement(){
     return (displacement/mRowHeight)/amount;
 }
 
-int ICCAD2017SolutionQuality::maximumCellMovement(){
+int ICCAD2017SolutionQuality::maximumCellMovement() {
     double maximumCellMovement = 0;
     ophidian::circuit::Cell cellID;
-    for(auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++){
-        if(cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt]) >= maximumCellMovement){
-        maximumCellMovement = cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt]);
-        cellID = *cellIt;
+
+    for (auto cell : mDesign.netlist().range(circuit::Cell())) {
+        if (cellDisplacement(mDesign.placement().cellLocation(cell), mInitialLocations[cell]) >= maximumCellMovement) {
+            maximumCellMovement = cellDisplacement(mDesign.placement().cellLocation(cell), mInitialLocations[cell]);
+            cellID = cell;
         }
     }
-        std::cout<<"Cell Name:"<<mDesign.netlist().name(cellID)<<std::endl;
+
     return maximumCellMovement / mRowHeight;
 }
 
-float ICCAD2017SolutionQuality::fmm(){
-    std::unordered_map<std::string, unsigned int> maximumMovement = {{"des_perf_b_md1", 300}, {"des_perf_b_md2", 300}, {"edit_dist_1_md1", 15}, {"edit_dist_a_md2", 400}, {"fft_2_md2", 20}, {"fft_a_md2", 400}, {"fft_a_md3", 20}, {"pci_bridge32_a_md1", 200}};
+double ICCAD2017SolutionQuality::fmm() {
     std::string circuitName = mDesign.circuitName();
     double displacement = 0;
-    for(auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++)
-        if(cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt])/mRowHeight > maximumMovement[circuitName])
+    for (auto cellIt = mDesign.netlist().begin(ophidian::circuit::Cell()); cellIt != mDesign.netlist().end(ophidian::circuit::Cell()); cellIt++)
+        if (cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt])/mRowHeight > mMaximumMovement[circuitName])
             displacement += cellDisplacement(mDesign.placement().cellLocation(*cellIt), mInitialLocations[*cellIt]);
-    displacement = (displacement / mRowHeight) / maximumMovement[circuitName];
+    displacement = (displacement / mRowHeight) / mMaximumMovement[circuitName];
     return std::max(displacement, 1.0);
 }
 
-double ICCAD2017SolutionQuality::hpwl(ophidian::design::Design & design, const ophidian::circuit::Net & net){
+double ICCAD2017SolutionQuality::fof() {
+    // TODO
+    auto singleBinArea = 64 * mRowHeight;
+
+    double totalOverflow = 1.0;
+    double dMax = 1.0;
+    double totalAreaMovableCells = 1.0;
+
+    return totalOverflow * singleBinArea * dMax * totalAreaMovableCells;
+}
+
+double ICCAD2017SolutionQuality::hpwl(ophidian::design::Design & design, const ophidian::circuit::Net & net) {
     double minX = std::numeric_limits<double>::max(),
            minY = std::numeric_limits<double>::max(),
            maxX = std::numeric_limits<double>::lowest(),
            maxY = std::numeric_limits<double>::lowest();
 
-    for(auto pin : design.netlist().pins(net)){
+    for (auto pin : design.netlist().pins(net)) {
         util::Location pinLocation;
 
         auto input = design.netlist().input(pin);
@@ -133,7 +146,7 @@ double ICCAD2017SolutionQuality::hpwl(ophidian::design::Design & design, const o
 }
 
 
-double ICCAD2017SolutionQuality::hpwl(const circuit::Net &net){
+double ICCAD2017SolutionQuality::hpwl(const circuit::Net &net) {
     return hpwl(mDesign, net);
 }
 
--- ophidian/legalization/iccad2017SolutionQuality.h
+++ ophidian/legalization/iccad2017SolutionQuality.h
@@ -3,6 +3,8 @@
 
 #include <ophidian/design/Design.h>
 
+#define DEBUG(x) [&]() { std::cerr << #x << ":" << x << std::endl; }()
+
 namespace ophidian
 {
 namespace legalization
@@ -11,7 +13,17 @@ class ICCAD2017SolutionQuality{
 public:
     ICCAD2017SolutionQuality(ophidian::design::Design &design, ophidian::design::Design &originalDesign);
 
-    float rawScore(){
+    double avgMovementScore();
+    double maxMovementScore();
+    double hpwlScore();
+    double softConstraintScore() { return 0.2; }
+    double runtimeScore() { return -0.2; }
+    double rawScore(){
+        DEBUG(avgMovementScore());
+        DEBUG(maxMovementScore());
+        DEBUG(hpwlScore());
+        DEBUG(softConstraintScore());
+        DEBUG(runtimeScore());
         return avgMovementScore() * maxMovementScore() * (1 + hpwlScore() + softConstraintScore()) * (1 + runtimeScore());
     }
 
@@ -22,17 +34,12 @@ public:
     int maximumCellMovement();
 
 private:
-    float avgMovementScore();
-    float maxMovementScore();
-    float hpwlScore();
-    float softConstraintScore(){return 0.2;}
-    float runtimeScore(){return -0.2;}
-
     double cellDisplacement(util::Location l1, util::Location l2){
         return std::abs(l1.toPoint().y() - l2.toPoint().y()) + std::abs(l1.toPoint().x() - l2.toPoint().x());
     }
 
-    float fmm();
+    double fmm();
+    double fof();
     double hpwl(const ophidian::circuit::Net & net);
     double hpwl(ophidian::design::Design & design, const ophidian::circuit::Net & net);
 
@@ -40,7 +47,20 @@ private:
     ophidian::entity_system::Property<ophidian::circuit::Cell, bool> mInitialFixed;
     ophidian::design::Design & mDesign;
     ophidian::design::Design & mOriginalDesign;
-    double mRowHeight;
+    const double mRowHeight;
+
+    const std::unordered_map<std::string, unsigned int> mMaximumMovement = {
+        {"des_perf_b_md1", 300},
+        {"des_perf_b_md2", 300},
+        {"edit_dist_1_md1", 15},
+        {"edit_dist_a_md2", 400},
+        {"fft_2_md2", 20},
+        {"fft_a_md2", 400},
+        {"fft_a_md3", 20},
+        {"pci_bridge32_a_md1", 200}
+    };
+
+    const double BETA = 1.0;
 };
 
 
